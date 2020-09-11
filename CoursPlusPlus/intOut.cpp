2fe0da3513fa2f86cc7f5933a2ba3310ad3deb8d
alancipher <alansame86@gmail.com>
2020-09-10 22:07:47 -0400

    commit sur les enttree sortie

---
+++ CoursPlusPlus/intOut.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char** argv){
+int a,b;
+cout << "Entrez deux nombres:" << endl;
+cin >>a>>b;
+int somme = a+b;
+cout << "La somme est :"<< somme << endl;
+return 0;
+}
