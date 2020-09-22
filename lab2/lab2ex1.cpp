5453caebd976b2f41d069af8b61642674f8f8c8b
alancipher <gustavealan@yahoo.com>
2020-09-22 04:42:13 -0400

    lab2

---
+++ lab2/lab2ex1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=1, b=2, c=3, d=4;//  declaration des entier a,b,c,d
+    int* pa=&a;        // declaction et initialisation du pointeur pa 
+    int* pb=&b;   // declaration et initialisation du pointeur pb  vers b
+    int* pc=&c, *pd=&d;
+    cout << "a=" << a << "\tb=" << b << "\tc=" << c << "\td=" << d << endl;
+    // affichage de a, b,c, d
+    cout << "pa=" << pa << "\tpb=" << pb << "\tpc=" << pc << "\tpd=" << pd << endl;
+   // affichage des pointeurs pa pb et pc et pd
+ *pa = 4; // dereferencement de pa
+  
+    cout << "a=" << a << "\tb=" << b << "\tc=" << c << "\td=" << d << endl;
+    cout << "pa=" << pa << "\tpb=" << pb << "\tpc=" << pc << "\tpd=" << pd << endl;
+     
+      pa = pb;// pa pointe desormais vers pb 
+    *pa = 8; // dereferencement de pa qui prend 8
+    cout << "a=" << a << "\tb=" << b << "\tc=" << c << "\td=" << d << endl;
+    cout << "pa=" << pa << "\tpb=" << pb << "\tpc=" << pc << "\tpd=" << pd << endl;
+    c = 10;
+    d += *pd;
+    cout << "a=" << a << "\tb=" << b << "\tc=" << c << "\td=" << d << endl;
+    cout << "pa=" << pa << "\tpb=" << pb << "\tpc=" << pc << "\tpd=" << pd << endl;
+    return 0;
+}
